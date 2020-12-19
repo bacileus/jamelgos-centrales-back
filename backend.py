d580199d5977ed4d37014a5a7bb00d7088f0fe89
Rey_PLEB <miquelgv1999@gmail.com>
2020-12-19 20:43:16 +0100

    Troliao Puto

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS, cross_origin
 import json
 from flask import Flask, jsonify, request, send_file, render_template, redirect
 from flask_pymongo import PyMongo
@@ -7,6 +8,8 @@ import hashlib
 
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 client = MongoClient("mongodb+srv://admin:Jamau@hackathon.0lxfu.mongodb.net/<dbname>?retryWrites=true&w=majority")
 dbInfected= client.get_database('COVID-Infected')
 infectados = dbInfected.Infected
