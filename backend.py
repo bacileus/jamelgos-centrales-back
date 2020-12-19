2f4acdd09846da3e534cba5d84a6f1895f743216
Rey_PLEB <miquelgv1999@gmail.com>
2020-12-19 22:58:19 +0100

    Troliao Puto

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask_cors import CORS, cross_origin
+
 import json
 from flask import Flask, jsonify, request, send_file, render_template, redirect
 from flask_pymongo import PyMongo
@@ -8,8 +8,7 @@ import hashlib
 
 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+
 client = MongoClient("mongodb+srv://admin:Jamau@hackathon.0lxfu.mongodb.net/<dbname>?retryWrites=true&w=majority")
 dbInfected= client.get_database('COVID-Infected')
 infectados = dbInfected.Infected
@@ -32,7 +31,12 @@ def registrarInfectado():
         return jsonify(response),406
 
 
-
+@app.route('/usuarios',methods=['GET'])
+def getUser():
+    mac = "AAA"
+    existsOne = infectados.Mac.find({"MAC":Mac})
+    myvar = "the answer is {}".format(existsOne)
+    return myvar
 
 #funcion para devolver toda la tabla
 
