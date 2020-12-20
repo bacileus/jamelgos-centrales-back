23fa8de6e47862a18df0551faa021d3b2e535012
bacileus <galejandrerh7@gmail.com>
2020-12-20 09:28:01 +0100

    new date format

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def getUsers():
 @app.route('/users',methods=['POST'])
 def postUser():
     MAC = request.get_json().get("MAC")
-    usuerInser = { "_id": MAC, "MAC": MAC, "noticedTime": datetime.now().strftime("%Y/%m/%d) }
+    usuerInser = { "_id": MAC, "MAC": MAC, "noticedTime": datetime.now().strftime("%Y/%m/%d") }
     colInfected.insert_one(usuerInser)
     response = {"response":"registered"}
     return flask.jsonify(response),201
